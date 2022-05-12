aba928b10f598e68e797f8abcd7b6da5ef507b96
Delmar-Ty <reliablesource13@gmail.com>
2022-05-11 18:27:04 -0700

    Made more animations and stuff

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,1 +1,8 @@
-//Script
+const wipeDiv = document.querySelector('.wipe');
+
+function wipe() {
+    wipeDiv.className = 'wipe start-wipe';
+    setTimeout(function() {wipeDiv.className = 'wipe'}, 3000);
+}
+
+document.querySelector('.my-name').addEventListener('click', wipe);
